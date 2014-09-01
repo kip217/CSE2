5c0ff452ec821063f5fb84f222eabc55a4f95d3e
kip217 <kip217@lehigh.edu>
2014-09-01 18:39:37 +0000

    created WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+///////////////////////////////////////////
+//  Katherine Porfirio
+//  08/30/2014
+//  CSE 002 Section 110
+//  Welcome Class Java Program
+//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+public static void main(String[] args) {
+
+//  print the statement
+    System.out.println("    _____________     ");
+    System.out.println("    |  WELCOME  |     ");
+    System.out.println("    _____________     ");
+    System.out.println("   ^  ^  ^  ^  ^  ^   ");            
+    System.out.println("  / |/ |/ |/ |/ |/ |  "); // '|' used instead of '\' because an error statement occurs when '\' is used
+    System.out.println(" |-K--I--P--2--1--7-| ");
+    System.out.println("  | /| /| /| /| /| /  ");
+    System.out.println("   v  v  v  v  v  v   "); // '|" used instead of '\' because an error statement occurs when '\' is used
+    System.out.println(" I am a sophomore studying electrical engineering. Last year my plane got struck by lightning when I was going home to Arizona.");
+    
+    }
+}
