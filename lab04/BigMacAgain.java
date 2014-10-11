d1cf40967e917acca3136e80e4df2881642eaa5f
kip217 <kip217@lehigh.edu>
2014-10-11 02:15:06 +0000

    fixed BigMacAgain

--- lab04/BigMacAgain.java
+++ lab04/BigMacAgain.java
@@ -26,7 +26,9 @@ public class BigMacAgain {
                 
                 //  calculate the cost of the big macs and print the subtotal
                 double bigMacCost = ((double) numberOfBigMacs) * 2.22;
-                System.out.println("You ordered " +numberOfBigMacs+ " Big Macs for a subtotal of $" +bigMacCost);
+                int intBigMacCost = (int) (bigMacCost * 100);
+                double doubleBigMacCost = ((double) intBigMacCost) / 100;
+                System.out.println("You ordered " +numberOfBigMacs+ " Big Macs for a subtotal of $" +doubleBigMacCost);
                 
                 //  determine if the user wants fries
                 System.out.println("");
@@ -36,11 +38,14 @@ public class BigMacAgain {
                 System.out.println("");
                 System.out.print("Please enter your choice: ");
                 
+                //  create a scanner object
+                Scanner input2 = new Scanner(System.in);
+                
                 //  if the input is a string
-                if (input.hasNextLine())    {
+                if (input2.hasNextLine())    {
                     
                     //  accept user input
-                    String friesDecision = input.nextLine();
+                    String friesDecision = input2.nextLine();
 
                     //  check the length of the string
                     int length = friesDecision.length();
@@ -49,18 +54,22 @@ public class BigMacAgain {
                     if (length==1)  {
                         
                         //  if the user wants fries
+                        
+                        int costWithFries = (int) ((doubleBigMacCost + 2.15) * 100);
+                        double realCostWithFries = ((double) costWithFries) / 100;
+                        
                         if (friesDecision.equals("Y") || friesDecision.equals("y")) {
                             //  print subtotal of fries and total cost of the order
                             System.out.println("You ordered 1 order of fries for a subtotal of $2.15");
                             System.out.println("");
-                            System.out.println("The total cost is: $" +(bigMacCost+2.15));
+                            System.out.println("The total cost is: $" +realCostWithFries);
                         }   //  end of if statement if the user wants fries
                         
                         //  if the user does not want fries
                         else if (friesDecision.equals("N") || friesDecision.equals("n"))    {
                             //  print total cost of the order
                             System.out.println("");
-                            System.out.println("The total cost is: $" +bigMacCost);
+                            System.out.println("The total cost is: $" +doubleBigMacCost);
                         }   //  end of else if statement if the user does not want fries
                         
                         //  if the user did not enter a valid character
